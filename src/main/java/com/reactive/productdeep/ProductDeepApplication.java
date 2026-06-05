4158f2d753cde199fb5335907d71dfd44a32c430
Prince BOGNONKPEclear <princedev317@gmail.com>
2026-06-05 13:40:01 +0100

    first commit

---
+++ src/main/java/com/reactive/productdeep/ProductDeepApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.productdeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductDeepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductDeepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reactive/productdeep/ProductDeepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.productdeep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductDeepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
